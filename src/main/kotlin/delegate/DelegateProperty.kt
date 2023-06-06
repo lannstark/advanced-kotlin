19a27698f888de46bd767581e348802e7ff84778
최태현 <choi-0202@kaist.ac.kr>
2023-06-06 14:14:04 +0900

    11강. 위임과 관련된 몇 가지 추가 기능

--- src/main/kotlin/delegate/DelegateProperty.kt
+++ src/main/kotlin/delegate/DelegateProperty.kt
@@ -1,17 +1,31 @@
 package delegate
 
 import kotlin.properties.Delegates
+import kotlin.properties.PropertyDelegateProvider
+import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-// by lazy()
 class Person3 {
-  val name: String by lazy {
+  val name: String by LazyInitProperty {
     Thread.sleep(2_000)
     "김수한무"
   }
+
+  val status: String by object : ReadOnlyProperty<Person3, String> {
+    private var isGreen: Boolean = false
+    override fun getValue(thisRef: Person3, property: KProperty<*>): String {
+      return if (isGreen) {
+        isGreen = false
+        "Happy"
+      } else {
+        isGreen = true
+        "Sad"
+      }
+    }
+  }
 }
 
-class LazyInitProperty<T>(val init: () -> T) {
+class LazyInitProperty<T>(val init: () -> T): ReadOnlyProperty<Any, T> {
   private var _value: T? = null
   private val value: T
     get() {
@@ -21,7 +35,7 @@ class LazyInitProperty<T>(val init: () -> T) {
       return _value!!
     }
 
-  operator fun getValue(thisRef: Any, property: KProperty<*>): T {
+  override fun getValue(thisRef: Any, property: KProperty<*>): T {
     return value
   }
 }
@@ -33,7 +47,69 @@ class Person4 {
 }
 
 fun main() {
-  val p = Person4()
-  p.age = 30
-  p.age = 30
+  Person5()
+}
+
+class Person5 {
+  val name by DelegateProvider("최태현")
+  val country by DelegateProvider("한국")
+}
+
+class DelegateProvider(
+  private val initValue: String
+) : PropertyDelegateProvider<Any, DelegateProperty> {
+  override fun provideDelegate(thisRef: Any, property: KProperty<*>): DelegateProperty {
+    if (property.name != "name") {
+      throw IllegalArgumentException("${property.name}은 안되요! name만 연결 가능합니다!")
+    }
+    return DelegateProperty(initValue)
+  }
+}
+
+class DelegateProperty(
+  private val initValue: String,
+) : ReadOnlyProperty<Any, String> {
+  override fun getValue(thisRef: Any, property: KProperty<*>): String {
+    return initValue
+  }
+}
+
+
+interface Fruit {
+  val name: String
+  val color: String
+  fun bite()
+}
+
+open class Apple : Fruit {
+  override val name: String
+    get() = "사과"
+  override val color: String
+    get() = "빨간색"
+  override fun bite() {
+    print("사과 톡톡~ 아삭~")
+  }
+}
+
+class GreenApple : Fruit {
+  override val name: String
+    get() = "사과"
+  override val color: String
+    get() = "초록색"
+
+  override fun bite() {
+    print("사과 톡톡~ 아삭~")
+  }
+}
+
+class GreenApple2 : Apple() {
+  override val color: String
+    get() = "초록색"
+}
+
+class GreenApple3(
+  private val apple: Apple
+) : Fruit by apple {
+  override val color: String
+    get() = "초록색"
 }
